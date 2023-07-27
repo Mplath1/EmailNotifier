021fdd256471b4dbaf87bce63f96744f00e6ffcf
Michael Plath <Accounting2@usawine.local>
2023-07-27 14:32:06 -0400

    added logging for data write. added loop to clear template before data write begins

--- src/main/java/model/Attachment.java
+++ src/main/java/model/Attachment.java
@@ -18,11 +18,13 @@ import java.util.concurrent.Callable;
 public class Attachment implements Callable {
     private static final Logger log = LoggerFactory.getLogger(Attachment.class);
 
+    @Deprecated
     private Line theLine;
     private String templateFileName;
     public Workbook theWorkbook;
     private Customer customer;
 
+    @Deprecated
     public Attachment(Line theLine,String templateFileName){
         this.theLine = theLine;
         this.templateFileName = templateFileName;
@@ -46,6 +48,7 @@ public class Attachment implements Callable {
         return outputFile;
     }
 
+    @Deprecated
     Workbook createAttachment(Line theLine, String theFile) {
         try {
             FileInputStream fis = new FileInputStream(new File(theFile));
@@ -88,29 +91,52 @@ public class Attachment implements Callable {
             File fis = new File(filePath);
             Workbook attachmentWorkbook = new XSSFWorkbook(fis);
             Sheet currentSheet = attachmentWorkbook.getSheetAt(0);
+            //TODO: clear all data (but not formatting) from Rows 22-26. possibly break in clearTemplate(template, rowRange)
+            for(int i = 22; i <27; i++){
+                Row currentRow = currentSheet.getRow(i);
+                currentRow.getCell(0).setCellValue("");
+                currentRow.getCell(1).setCellValue("");
+                currentRow.getCell(2).setCellValue("");
+                currentRow.getCell(3).setCellValue("");
+                currentRow.getCell(4).setCellValue("");
+                currentRow.getCell(5).setCellValue("");
+                currentRow.getCell(6).setCellValue("");
+                currentRow.getCell(7).setCellValue("");
+            }
             int startingRow = 22;
             Row currentRow = currentSheet.getRow(startingRow);
 
             for (int i = 0; i < customer.getInvoiceList().size(); i++) {
             Invoice currentInvoice = customer.getInvoiceList().get(i);
+            StringBuilder sb = new StringBuilder(startingRow + ":");
             currentRow = currentSheet.getRow(startingRow);
             Cell currentCell = currentRow.getCell(0);
             currentCell.setCellValue(currentInvoice.getOrderNumber());
+            sb.append(currentInvoice.getOrderNumber() + ",");
             currentCell = currentRow.getCell(1);
             currentCell.setCellValue(currentInvoice.getDeliveryDate());
+            sb.append(currentInvoice.getDeliveryDate() + ",");
             currentCell = currentRow.getCell(2);
             currentCell.setCellValue(currentInvoice.getNotificationDate());
+            sb.append(currentInvoice.getNotificationDate() + ",");
             currentCell = currentRow.getCell(3);
             currentCell.setCellValue(currentInvoice.getReportingDate());
+            sb.append(currentInvoice.getReportingDate() + ",");
             currentCell = currentRow.getCell(4);
             currentCell.setCellValue(currentInvoice.getPortfolioCode());
+            sb.append(currentInvoice.getPortfolioCode() + ",");
             currentCell = currentRow.getCell(5);
             currentCell.setCellValue(customer.getLicenseNumber()); //TODO: being set as number instead of text
+                sb.append(customer.getLicenseNumber().toString() + ",");
             currentCell = currentRow.getCell(6);
             currentCell.setCellValue(customer.getCustomerName());
+                sb.append(customer.getCustomerName() + ",");
             currentCell = currentRow.getCell(7);
             currentCell.setCellValue("$" + currentInvoice.getDollarValue());
+                sb.append("$" + currentInvoice.getDollarValue());
+                log.debug("{}",sb.toString());
             startingRow++;
+
             };
             //TODO: seperate into new private method
             String tempOutFile = "src/resources/AttachmentTemplates";
